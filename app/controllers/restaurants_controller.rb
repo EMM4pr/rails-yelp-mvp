4ddc5248d43502c187c80284a499591aa2359206
emmapreysing <emma@preysingmail.com>
2023-11-15 13:40:01 +0100

    Generate Models & Controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,68 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show edit update destroy]
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurant/top
+  def top
+    @top_restaurants = Restaurant.where(rating: 5)
+  end
+
+  # GET /restaurants/1
+  def show
+  end
+
+  # GET /restaunts/1/chef
+  def chef
+    @restaurant = Restaurant.find(params[:id])
+    @chef_info = @restaurant.chef_name
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.', status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /restaurants/1
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.', status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
